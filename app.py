a2fefdfc403f5ea940c4f62c68e2305877548023
praise2112 <oketola.praise@gmail.com>
2020-02-20 19:06:25 +0700

    fixing cors attempt 4

--- app.py
+++ app.py
@@ -6,73 +6,21 @@ from keras.models import load_model
 from numpy import newaxis
 import numpy as np
 
-
 from PIL import Image, ImageFilter
-
 
 app = Flask(__name__)
 CORS(app)
+
+
 # CORS(app, support_credentials=True)
 
 
-
-from datetime import timedelta
-from flask import make_response, request, current_app
-from functools import update_wrapper
-
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
-                attach_to_all=True, automatic_options=True):
-    """Decorator function that allows crossdomain requests.
-      Courtesy of
-      https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
-    """
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    # use str instead of basestring if using Python 3.x
-    if headers is not None and not isinstance(headers, str):
-        headers = ', '.join(x.upper() for x in headers)
-    # use str instead of basestring if using Python 3.x
-    if not isinstance(origin, str):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        """ Determines which methods are allowed
-        """
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        """The decorator function
-        """
-        def wrapped_function(*args, **kwargs):
-            """Caries out the actual cross domain code
-            """
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            h['Access-Control-Allow-Credentials'] = 'true'
-            h['Access-Control-Allow-Headers'] = \
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 
 mnist_model = load_model("model/keras_mnist_2Layer_adam_128BS_20epochs.h5", compile=False)
@@ -116,18 +64,19 @@ def imageprepare(argv):
     return tva
 
 
-
 def crop(png_image_name):
-     im = Image.open(png_image_name)
-     print(im.size)
+    im = Image.open(png_image_name)
+    print(im.size)
     # (364, 471)
-     print(im.getbbox())
+    print(im.getbbox())
     # (64, 89, 278, 267)
-     x, y, z, a = im.getbbox()
-     im2 = im.crop((x-40, y-20, z+40, a+20))    # show have put im.getbbox() here as an argument, but we get better accuracy when there is a margin around
-     print(im2.size)
+    x, y, z, a = im.getbbox()
+    im2 = im.crop((x - 40, y - 20, z + 40,
+                   a + 20))  # show have put im.getbbox() here as an argument, but we get better accuracy when there is a margin around
+    print(im2.size)
     # (214, 178)
-     im2.save("upload_folder/test.png")
+    im2.save("upload_folder/test.png")
+
 
 @app.route('/')
 def hello_world():
@@ -136,7 +85,6 @@ def hello_world():
 
 @app.route('/predict', methods=['POST', 'GET', 'OPTIONS'])
 # @cross_origin(supports_credentials=True)
-@crossdomain(origin='*')
 def predict():
     file = request.files['file']
     filename = secure_filename(file.filename)
@@ -145,7 +93,8 @@ def predict():
 
     virtualPath = "upload_folder/" + filename
 
-    crop("upload_folder/" + filename)   # crop image to the size of the number. cropping the transparent spaces way from the png increases the accuracy because of better quality
+    crop(
+        "upload_folder/" + filename)  # crop image to the size of the number. cropping the transparent spaces way from the png increases the accuracy because of better quality
     # crop save image at "upload_folder/test.png"
 
     img_ = Image.open("upload_folder/test.png")  # image extension *.png,*.jpg
@@ -171,7 +120,7 @@ def predict():
     # convert  the 28 x 28 image to mnist format
     x_ = imageprepare('temp/temp.png')  # file path here
     print(len(x_))  # mnist IMAGES are 28x28=784 pixels
-    X_ = np.asarray(x_, dtype=np.float32)   # convert to numpy array
+    X_ = np.asarray(x_, dtype=np.float32)  # convert to numpy array
 
     # using our model to make predictions
     import keras.backend.tensorflow_backend as tb
